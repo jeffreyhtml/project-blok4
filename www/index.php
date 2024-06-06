43458eaebb05d7b90b1608e2030b81a3d36d1860
Jeffrey van Zon <191317@novacollege.nl>
2024-06-06 09:19:11 +0200

    Begonnen met de index

--- www/index.php
+++ www/index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
+    <title>MyHolidays</title>
+    <link rel="icon" href="C:\Project\project-blok4\images\logo.png" type="image/icon type">
+    <link rel="stylesheet" href="style.css">
+
 </head>
+
 <body>
-    
+
+    <header>
+        <img src="images/logo.png" alt="logo">
+    </header>
+<nav class="navbar">
+        <a href="holidays.php">Overzicht vakanties</a>
+        <a href="login.php">Inloggen</a>
+        <a href="logout.php">Uitloggen</a>
+        <a href="registration.php">Registratie</a>
+</nav>
 </body>
+
 </html>
